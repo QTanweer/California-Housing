58192e7a58119fb6b5460e355aae9685cdf7ef65
Qamar Tanweer <qtanweer.mts41ceme@gmail.com>
2023-02-28 20:20:33 +0500

    ficed 'predict' method

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('home.html', prediction_text='The predicted house price is {}'.format(output))
+    return render_template('predict.html', prediction_text='The predicted house price is {}'.format(output))
 
 
 
